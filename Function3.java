370af0b1617e1d41a13b243ed362b7af7fe3e362
ShaoYuLiu <liebepiepiecc@gmail.com>
2014-03-14 16:58:25 +0800

    cos(x) Function class
    the class to calculate cos(x)

---
+++ Function3.java
@@ -0,0 +1,10 @@
+// a class that can calculate cos(x)
+public class Function3 extends AbstractDrawFunction{
+	public Function3() {
+		// TODO Auto-generated constructor stub
+	}
+	// a calculat method	
+	public double f (double x){
+		return Math.cos(x);
+	}
+}
