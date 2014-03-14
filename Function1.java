aa4722345ba32d2ada458b1e59d42caa6c01d1fb
ShaoYuLiu <liebepiepiecc@gmail.com>
2014-03-14 16:56:07 +0800

    x*2 Function class
    the class to calculate x*2

---
+++ Function1.java
@@ -0,0 +1,10 @@
+// a class that can calculate x*x
+public class Function1 extends AbstractDrawFunction{
+	public Function1() {
+		// TODO Auto-generated constructor stub
+	}
+	// a calculat method
+	public double f(double x){
+		return Math.pow( x, 2);
+	}
+}
