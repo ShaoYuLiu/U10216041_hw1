7b5bf47052b6b2a47d23671d46e44b5473ddb411
ShaoYuLiu <liebepiepiecc@gmail.com>
2014-03-14 16:59:57 +0800

    cos(x)+5sin(x) Function class
    the class to calculate cos(x)+5sin(x)

---
+++ Function5.java
@@ -0,0 +1,10 @@
+// a class that can calculate cos(x)+5sin(x)
+public class Function5 extends AbstractDrawFunction{
+	public Function5() {
+		// TODO Auto-generated constructor stub
+	}
+	// a calculat method
+	public double f (double x){
+		return Math.cos(x) + 5 * Math.sin(x);
+	}
+}
