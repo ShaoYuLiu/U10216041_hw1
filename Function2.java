2243c9f420c7353d57ab5f144efd1e73903bc719
ShaoYuLiu <liebepiepiecc@gmail.com>
2014-03-14 16:58:03 +0800

    sin(x) Function class
    the class to calculate sin(x)

---
+++ Function2.java
@@ -0,0 +1,10 @@
+// a class that can calculate sin(x)
+public class Function2 extends AbstractDrawFunction{
+	public Function2() {
+		// TODO Auto-generated constructor stub
+	}
+	// a calculat method
+	public double f(double x){
+		return Math.sin(x);
+	}
+}
