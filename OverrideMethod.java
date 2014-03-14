0f306fbac68e251e5b48031f0ad62006b715706b
ShaoYuLiu <liebepiepiecc@gmail.com>
2014-03-14 17:01:49 +0800

    Override class
    Override method

---
+++ OverrideMethod.java
@@ -0,0 +1,8 @@
+// create a class that extends the AbstractDrawFunction
+public class OverrideMethod extends AbstractDrawFunction{
+	@Override
+	// a method can be used hen it has an argument
+	double f(double x){
+		return x;
+	}
+}
