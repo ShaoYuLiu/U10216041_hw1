14e7a0446c056bbaa110129b06998ead8efe6d87
ShaoYuLiu <liebepiepiecc@gmail.com>
2014-03-14 17:01:08 +0800

    log(x)+x*x Function class
    the class to calculate log(x)+x*x

---
+++ Function7.java
@@ -0,0 +1,10 @@
+// a class that can calculate log(x)+x^2
+public class Function7 extends AbstractDrawFunction{
+	public Function7() {
+		// TODO Auto-generated constructor stub
+	}
+	// a calculat method
+	public double f (double x){
+		return Math.log(x) + Math.pow(x , 2);
+	}
+}
