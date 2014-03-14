26da548c0dfcee292347ff91328548c9471126f9
ShaoYuLiu <liebepiepiecc@gmail.com>
2014-03-14 16:59:22 +0800

    tan(x) Function class
    the class to calculate tan(x)

---
+++ Function4.java
@@ -0,0 +1,10 @@
+// a class that can calculate tan(x)
+public class Function4 extends AbstractDrawFunction{
+	public Function4() {
+		// TODO Auto-generated constructor stub
+	}
+	// a calculat method
+	public double f (double x){
+		return Math.tan(x);
+	}
+}
